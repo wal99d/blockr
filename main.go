da3112b2149d0248b79b19bbe388f37751b0c85c
wal99d <i@waleed.digital>
2024-10-06 08:32:36 +0300

    Peer to peer with GRPC 1...

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"net"
 	"time"
 
 	"github.com/wal99d/blockr/node"
@@ -14,14 +12,7 @@ import (
 
 func main() {
 	node := node.NewNode()
-	opts := []grpc.ServerOption{}
-	grpcServer := grpc.NewServer(opts...)
-	ln, err := net.Listen("tcp", ":3000")
-	if err != nil {
-		log.Fatal(err)
-	}
-	proto.RegisterNodeServer(grpcServer, node)
-	fmt.Println("node running on port:", "3000")
+
 	go func() {
 
 		for {
@@ -30,8 +21,8 @@ func main() {
 			makeTransaction()
 		}
 	}()
-	grpcServer.Serve(ln)
 
+	log.Fatal(node.Start(":3000"))
 }
 
 func makeTransaction() {
--- node/node.go
+++ node/node.go
@@ -3,21 +3,52 @@ package node
 import (
 	"context"
 	"fmt"
+	"net"
+	"sync"
 
 	"github.com/wal99d/blockr/proto"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/peer"
 )
 
 type Node struct {
-	version string
+	peerLock sync.RWMutex
+	peers    map[proto.NodeClient]bool
+	version  string
 	proto.UnimplementedNodeServer
 }
 
 func NewNode() *Node {
 	return &Node{
-
+		peers:   make(map[proto.NodeClient]bool),
 		version: "blockr-0.1",
 	}
+}
+
+func (n *Node) addPeer(c proto.NodeClient) {
+
+	n.peerLock.Lock()
+	defer n.peerLock.Unlock()
+	n.peers[c] = true
+}
+
+func (n *Node) deletePeer(c proto.NodeClient) {
+	n.peerLock.Lock()
+	defer n.peerLock.Unlock()
+	delete(n.peers, c)
+}
+
+func (n *Node) Start(listenAddr string) error {
+	opts := []grpc.ServerOption{}
+	grpcServer := grpc.NewServer(opts...)
+	ln, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		return err
+	}
+	proto.RegisterNodeServer(grpcServer, n)
+
+	fmt.Println("node running on port:", listenAddr)
+	return grpcServer.Serve(ln)
 }
 
 func (n *Node) Handshake(ctx context.Context, version *proto.Version) (*proto.Version, error) {
@@ -38,4 +69,14 @@ func (n *Node) HandleTransaction(ctx context.Context, tx *proto.Transaction) (*p
 	peer, _ := peer.FromContext(ctx)
 	fmt.Println("received tx from:", peer)
 	return &proto.Ack{}, nil
+}
+
+func makeNodeClient(listenAddr string) (proto.NodeClient, error) {
+
+	conn, err := grpc.NewClient(listenAddr, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+
+	return proto.NewNodeClient(conn), nil
 }
